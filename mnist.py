12709c9d1fa43ade84ecf5d3116a1fc496c27c69
Henrique <hpeluzio@gmail.com>
2023-04-11 16:25:36 -0300

    Creating dummy variables class_training

--- mnist.py
+++ mnist.py
@@ -9,4 +9,17 @@ from keras.layers import Conv2D, MaxPooling2D
 
 
 # plt.imshow(X_training[3]) # With color
-plt.imshow(X_training[3], cmap = 'gray')  # Without color
+plt.imshow(X_training[5], cmap = 'gray')  # Without color
+plt.title('Classe ' + str(y_training[5]))
+
+predictors_training = X_training.reshape(X_training.shape[0], 28, 28, 1)
+predictors_training = predictors_training.astype('float32')
+
+predictors_test = X_test.reshape(X_test.shape[0], 28, 28, 1)
+predictors_test = predictors_test.astype('float32')
+
+# Applying min-max normalization-
+predictors_training /= 255
+predictors_test /= 255
+
+training_class = np_utils.to_categorical(y_training, 10)
